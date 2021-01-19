e7c77ab061d826073588f52d77fc6ef45ebeef43
bcc0830 <bcc0830@naver.com>
2021-01-19 18:26:37 +0900

    2021.01.19

--- text_processing.py
+++ text_processing.py
@@ -4,15 +4,13 @@
 
 def normalize(input_string):
     normalized_string = ' '.join(input_string.rstrip().lstrip().split())
-    return normalized_string
-
+    return normalized_string.lower()
 
 def no_vowels(input_string):
-
     res = []
     for i in range(len(input_string)):
         is_vowel = False
-        for j in ['a', 'e', 'i', 'o', 'u']:
+        for j in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
             if input_string[i] == j:
                 is_vowel = True
                 break
